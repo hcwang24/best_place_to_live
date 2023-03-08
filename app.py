b7de7a471a7f9d0d99617d5165af96454608235b
hcwang24 <hcwang24@student.ubc.ca>
2023-03-08 08:50:32 -0800

    Base graph added

--- app.py
+++ app.py
@@ -1,34 +1,22 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, html, dcc
-import plotly.express as px
 import pandas as pd
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
 
+# Load the data
+data = pd.read_csv('van_houses.csv')
+
+# Create the app
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
+# Define the layout
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
+    html.H1(children='Vancouver Housing Values'),
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='housing-values',
+        figure=px.scatter(data, x='Geo Local Area', y='current_land_value', color='zoning_classification', title='Vancouver Housing Values')
     )
 ])
 
+# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
