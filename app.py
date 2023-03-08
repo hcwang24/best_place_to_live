dd4c8a7c18dc7b55e66b9c12f0ec9975e2d830e6
hcwang24 <hcwang24@student.ubc.ca>
2023-03-08 09:05:52 -0800

    Add checkboxes

--- app.py
+++ app.py
@@ -9,12 +9,33 @@ data = pd.read_csv('van_houses.csv')
 app = Dash(__name__)
 
 # Define the layout
-app.layout = html.Div(children=[
-    html.H1(children='Vancouver Housing Values'),
-    dcc.Graph(
-        id='housing-values',
-        figure=px.scatter(data, x='Geo Local Area', y='current_land_value', color='zoning_classification', title='Vancouver Housing Values')
-    )
+app.layout = html.Div([
+    html.Div(children=[
+        html.Label('Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal',
+                     'San Francisco'], 'Montréal'),
+
+        html.Br(),
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'],
+                     ['Montréal', 'San Francisco'],
+                     multi=True),
+
+        html.Br(),
+        html.Label('Radio Items'),
+        dcc.RadioItems(['New York City', 'Montréal',
+                       'San Francisco'], 'Montréal'),
+    ], style={'padding': 10, 'flex': 1}),
+
+    html.Div(
+        children=[
+            html.H1(children='Vancouver Housing Values'),
+            dcc.Graph(
+                id='housing-values',
+                figure=px.scatter(data, x='Geo Local Area', y='current_land_value',
+                                  color='zoning_classification', title='Vancouver Housing Values')
+            )
+        ])
 ])
 
 # Run the app
