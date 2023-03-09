307b159fc3a071d80141827e418f03690156e316
hcwang24 <hcwang24@student.ubc.ca>
2023-03-09 11:38:24 -0800

    Adding a map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
+import plotly.graph_objs as go
 
 # Load the data
 data = pd.read_csv('van_houses.csv')
@@ -17,17 +18,24 @@ app.layout = html.Div([
             options=[{'label': x, 'value': x} for x in sorted(list(set(data['Geo Local Area'])))],
             value='Marpole'
         )
-    ], style={'padding': 10, 'flex': 1}),
+    ], style={'padding': 10,'height': '200px'}),
 
     html.Div(
         children=[
             html.H1(children='Vancouver Housing Values'),
             dcc.Graph(
                 id='housing-values',
-                figure=px.scatter(data, x='zoning_classification', y='current_land_value',
-                                  color='zoning_classification', title='Vancouver Housing Values')
+                figure=px.box(data, x='Geo Local Area', y='current_land_value',
+                              color='zoning_classification', title='Vancouver Housing Values',
+                              boxmode='group', points=False)
             )
-        ])
+        ], style={'display': 'flex', 'flex-direction': 'row'}
+    ),
+
+    dcc.Graph(
+        id='map',
+        style={'width': '100%', 'height': '500px'}
+    )
 ])
 
 # Define callbacks
@@ -38,11 +46,23 @@ app.layout = html.Div([
 def update_graph(geo_value):
     filtered_data = data.loc[(data['Geo Local Area'] == geo_value)]
     fig = px.box(filtered_data, x='zoning_classification', y='current_land_value',
-                      color='zoning_classification', title='Vancouver Housing Values')
-    fig.update_xaxes(title='Community')
-    fig.update_yaxes(title='House Value ($)')
+                 color='zoning_classification', title='Vancouver Housing Values',points=False, height=500)
+    fig.update_xaxes(title='House Type')
+    fig.update_yaxes(title='Current Land Value')
+    return fig
+
+@app.callback(
+    Output('map', 'figure'),
+    Input('geo-dropdown', 'value')
+)
+def update_map(geo_value):
+    filtered_data = data.loc[(data['Geo Local Area'] == geo_value)]
+    fig = px.scatter_mapbox(filtered_data, lat='latitude', lon='longitude',
+                            hover_name='full_address', hover_data=['current_land_value'],
+                            zoom=11, height=500)
+    fig.update_layout(mapbox_style="open-street-map")
     return fig
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
