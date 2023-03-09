c503c7affe391160be11bfdfb6843418aa39c199
hcwang24 <hcwang24@student.ubc.ca>
2023-03-09 11:42:03 -0800

    Fixing the layout

--- app.py
+++ app.py
@@ -12,33 +12,35 @@ app = Dash(__name__)
 # Define the layout
 app.layout = html.Div([
     html.Div(children=[
+        html.H1(children='Vancouver Housing Values'),
         html.Label('Dropdown'),
         dcc.Dropdown(
             id='geo-dropdown',
-            options=[{'label': x, 'value': x} for x in sorted(list(set(data['Geo Local Area'])))],
+            options=[{'label': x, 'value': x}
+                     for x in sorted(list(set(data['Geo Local Area'])))],
             value='Marpole'
         )
-    ], style={'padding': 10,'height': '200px'}),
+    ], style={'padding': 10, 'flex': 1}),
 
     html.Div(
         children=[
-            html.H1(children='Vancouver Housing Values'),
             dcc.Graph(
                 id='housing-values',
                 figure=px.box(data, x='Geo Local Area', y='current_land_value',
                               color='zoning_classification', title='Vancouver Housing Values',
-                              boxmode='group', points=False)
+                              boxmode='group')
+            ),
+            dcc.Graph(
+                id='map',
+                style={'width': '100%', 'height': '500px', 'margin': '20px 0'}
             )
-        ], style={'display': 'flex', 'flex-direction': 'row'}
+        ], style={'display': 'flex', 'flex-direction': 'row', 'margin': '20px 0'}
     ),
-
-    dcc.Graph(
-        id='map',
-        style={'width': '100%', 'height': '500px'}
-    )
 ])
 
 # Define callbacks
+
+
 @app.callback(
     Output('housing-values', 'figure'),
     Input('geo-dropdown', 'value')
@@ -46,10 +48,11 @@ app.layout = html.Div([
 def update_graph(geo_value):
     filtered_data = data.loc[(data['Geo Local Area'] == geo_value)]
     fig = px.box(filtered_data, x='zoning_classification', y='current_land_value',
-                 color='zoning_classification', title='Vancouver Housing Values',points=False, height=500)
+                 color='zoning_classification', title='Vancouver Housing Values', height=500)
     fig.update_xaxes(title='House Type')
     fig.update_yaxes(title='Current Land Value')
     return fig
+
 
 @app.callback(
     Output('map', 'figure'),
@@ -62,6 +65,7 @@ def update_map(geo_value):
                             zoom=11, height=500)
     fig.update_layout(mapbox_style="open-street-map")
     return fig
+
 
 # Run the app
 if __name__ == '__main__':
